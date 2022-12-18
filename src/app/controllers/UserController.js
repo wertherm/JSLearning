db713369a524f87e65c0e06fea515710009793aa
w.sidonio <w.sidonio@ecotrace.info>
2022-12-18 10:41:43 -0300

    chore: simple adjust

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,10 @@
+//https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs/Introduction
+
 //Trecho ES6 que exporta um objeto com uma função "store".
 //Extrai os valores de "name", "email" e "password" do corpo da solicitação e os armazena em uma variável chamada "user".
 //Em seguida, a função retorna esses dados em formato JSON usando o método "json" da resposta.
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/export
+//https://bobbyhadz.com/blog/javascript-uncaught-syntaxerror-unexpected-token-export
 export default {
   async store(req, res) {
     const { name, email, password } = req.body;
@@ -18,8 +22,6 @@ export default {
 //Trecho ES5 que define uma classe "ModuloDeImpressao".
 //Essa classe tem um construtor que inicializa uma propriedade "_codigo" com o valor 10.
 //Também tem uma função "imprime" que imprime cada elemento de uma matriz de nomes, precedido pelo valor da propriedade "_codigo". Por fim, o módulo é exportado usando o método "module.exports". Isso permite que outros módulos importem e usem a classe "ModuloDeImpressao".
-//https://www.alura.com.br/artigos/utilizando-export-modules-no-node-js
-//https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs/Introduction
 class ModuloDeImpressao {
   constructor() {
     this._codigo = 10;
