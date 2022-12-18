6536b698a336c6b8c7a512ece886c5c27bc0102b
w.sidonio <w.sidonio@ecotrace.info>
2022-12-18 01:00:10 -0300

    chore: simple adjust

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
-/*
-ES6
+//Trecho ES6 que exporta um objeto com uma função "store".
+//Extrai os valores de "name", "email" e "password" do corpo da solicitação e os armazena em uma variável chamada "user".
+//Em seguida, a função retorna esses dados em formato JSON usando o método "json" da resposta.
 export default {
   async store(req, res) {
     const { name, email, password } = req.body;
@@ -13,19 +14,22 @@ export default {
     return res.json(user);
   }
 };
-*/
 
-//ES5
+//Trecho ES5 que define uma classe "ModuloDeImpressao".
+//Essa classe tem um construtor que inicializa uma propriedade "_codigo" com o valor 10.
+//Também tem uma função "imprime" que imprime cada elemento de uma matriz de nomes, precedido pelo valor da propriedade "_codigo". Por fim, o módulo é exportado usando o método "module.exports". Isso permite que outros módulos importem e usem a classe "ModuloDeImpressao".
 //https://www.alura.com.br/artigos/utilizando-export-modules-no-node-js
 //https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs/Introduction
 class ModuloDeImpressao {
   constructor() {
     this._codigo = 10;
-   }
+  }
+  
   imprime(nomes) {
     nomes.forEach(nome => {
         console.log(`${this._codigo}: ${nome}`);
-     });
-   }
+    });
+  }
 }
+
 module.exports = ModuloDeImpressao;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 //CLI (pós-clone):
 //npm install
+//npm start
+//curl localhost:3333
+//npm init -y - Só para caso o package.json não tenha sido criado
 
 //Precisa do módulo sucrase para utilizar 'import' (ES6) https://www.geeksforgeeks.org/how-to-use-an-es6-import-in-node-js
 //import express from 'express';
 const express = require('express');
-
-//ES6
-//const UserController = require('./app/controllers/UserController');
-//https://www.youtube.com/watch?v=uonKHztGhko
-
 const app = express();
 
 //ES6
-//app.post('/users', UserController.store); //Cria uma rota /users
+const UserController = require('./app/controllers/UserController');
+//https://www.youtube.com/watch?v=uonKHztGhko
+
+//ES6
+app.post('/users', UserController.store); //Cria rota /users
 
 app.listen(3333, () => {
-  console.log('Servidor executando em localhost:3333'); //Mostra tanto no terminal como no browser
+  console.log('Servidor executando em localhost:3333');
 })
